5e91e2a04981693ddb6bc91a1c286fcbce96ff71
Ulysse Lopez <ulysse.lopez@etu.univ-nantes.fr>
2023-12-09 16:33:19 +0100

    challenge 9 part 2 complete

--- 09/main.py
+++ 09/main.py
@@ -33,6 +33,34 @@ def solve1(sequences):
     return sum(all_values)
 
 
+def solve2(sequences):
+    all_values = []
+    for seq in range(len(sequences)):
+        first_values = [sequences[seq][0]]
+        init_seq = sequences[seq].copy()
+        
+        seq_end = False
+
+        while not seq_end:
+            diff_seq = []
+            seq_end = True
+            for i in range(1, len(init_seq)):
+                diff = init_seq[i] - init_seq[i-1]
+                diff_seq.append(diff)
+                if diff != 0:
+                    seq_end = False
+
+            first_values.append(diff_seq[0])
+            init_seq = diff_seq
+
+        s = 0
+        for v in range(len(first_values)-2, -1, -1):
+            s = first_values[v] - s
+        
+        all_values.append(s)
+    
+    return sum(all_values)
+
 sequences = parseFile('09/input')
 
-print(solve1(sequences))
+print(solve2(sequences))
